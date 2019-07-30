b4664c23518c42787037f2aa3fae374d451aef5d
Evans-N <51976431+Evans-N@users.noreply.github.com>
2019-07-30 14:19:04 -0400

    Update inspiration.js

--- InspirationAPI/inspiration.js
+++ InspirationAPI/inspiration.js
@@ -9,7 +9,6 @@ function* quoteGenerator() {
     		.then(res => res.json());
  	}
 }
-
 const generator = quoteGenerator();
 
 function updateQuote() {
@@ -20,12 +19,7 @@ function updateQuote() {
 		quoteButton.disabled = false;
 	});
 }
-
 quoteButton.onclick = updateQuote;
 
-<<<<<<< HEAD
 updateQuote();
-=======
 updateQuote();
-
->>>>>>> ff1ef1048d6e58f3244e908c21130e3aa815d881
